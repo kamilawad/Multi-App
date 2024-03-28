33c7254ad1cbc693c04edfc92a96581b635c64c2
kamilawad1 <kamilawad15@gmail.com>
2024-03-28 03:41:02 +0200

    linking to publuc api and getting weather data

--- src/pages/Weather/index.jsx
+++ src/pages/Weather/index.jsx
@@ -1,7 +1,35 @@
 import "./index.css";
 
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 const Weather = () => {
-    
+
+    useEffect(() => {
+        fetchWeatherData();
+    }, []);
+
+    const fetchWeatherData = async () => {
+        try {
+            const response = await axios.get('https://api.weatherapi.com/v1/forecast.json?key=fd3fe140d3de4bab809212205242703&q=beirut&days=4');
+            if (response.data) {
+                const data = response.data;
+                console.log(data);
+                const {name, country} = data.location;
+                const {is_day, temp_c, feelslike_c} = data.current;
+                data.forecast.forecastday.forEach(({ date, day, astro }) => {
+                    console.log(date)
+                    const  { maxtemp_c, mintemp_c, avghumidity, maxwind_kph, totalprecip_mm, daily_chance_of_rain } = day;
+                    const { sunrise, sunset } = astro;
+                    console.log( maxtemp_c, mintemp_c, avghumidity, maxwind_kph, totalprecip_mm, daily_chance_of_rain,sunrise, sunset);
+
+                });
+            }
+            
+        } catch (error) {
+          console.error('Error fetching posts:', error);
+        }
+    };
     return (
         <div>
             Weather
