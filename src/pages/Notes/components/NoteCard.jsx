2c9e2b97b5c4d7c38b11da9ea3b01e5ac180e8d9
kamilawad1 <kamilawad15@gmail.com>
2024-03-29 09:05:03 +0200

    added delete note and add note

--- src/pages/Notes/components/NoteCard.jsx
+++ src/pages/Notes/components/NoteCard.jsx
@@ -1,8 +1,11 @@
 
-const NoteCard = () => {
+const NoteCard = ({ id, text, handleDeleteNote }) => {
 	return (
 		<div className='note-card'>
 			<p>{text}</p>
+			<div className='note-footer'>
+				<button onClick={() => handleDeleteNote(id)}>Delete</button>
+			</div>
 		</div>
 	);
 };
--- src/pages/Notes/index.jsx
+++ src/pages/Notes/index.jsx
@@ -1,18 +1,45 @@
 import "./index.css";
 
+import NoteCard from "./components/NoteCard";
+
 import { useEffect, useState } from "react";
 
 const Notes = () => {
 
     const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) ?? []);
 
+    const [newNoteText, setNewNoteText] = useState("");
+
     useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(notes))
       }, [notes]);
+
+    const handleDeleteNote = (id) => {
+	    const newNotes = notes.filter((note) => note.id !== id);
+		setNotes(newNotes);
+	};
     
+    const handleAddNote = () => {
+		if (newNoteText.trim()) {
+            setNotes([...notes, { id: crypto.randomUUID(), text: newNoteText }]);
+            setNewNoteText("");
+          }
+	};
+
     return (
         <div className='container'>
-			
+			<div className='notes-list'>
+                {notes.map((note) => (
+                    <NoteCard
+                        id={note.id}
+                        text={note.text}
+                        handleDeleteNote={handleDeleteNote}
+                    />
+                ))}	
+		    </div>
+            <input type="text" value={newNoteText} onChange={(e) => setNewNoteText(e.target.value)}
+                placeholder="Enter your note" />
+            <button onClick={handleAddNote}>Add Note</button>
 		</div>
     );
 }
